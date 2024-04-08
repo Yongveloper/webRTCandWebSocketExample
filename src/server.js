5a9143f331a707200506cfc531c2db017e4c989f
yongveloper <aaa9015@naver.com>
2024-04-08 18:50:04 +0900

    WebSocket Events

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http';
+import WebSocket from 'ws';
 import express from 'express';
 
 const app = express();
@@ -8,6 +10,16 @@ app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render('home'));
 
-const handleListen = (port) =>
-  console.log(`💫 http://localhost:${port}에서 서버가 실행되었습니다. 💫`);
-app.listen(PORT, () => handleListen(PORT));
+const handleListen = () =>
+  console.log(`💫 http://localhost:${PORT}에서 서버가 실행되었습니다. 💫`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const handleConnection = (socket) => {
+  console.log(socket);
+};
+
+wss.on('connection', handleConnection);
+
+server.listen(3000, handleListen);
