b90d59aaba3fb81466eb49000ec07cc399ea8013
yongveloper <aaa9015@naver.com>
2024-04-12 23:40:43 +0900

    Nickname part One

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,14 +1,24 @@
 const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const messageForm = document.querySelector('#message');
+const nicknameForm = document.querySelector('#nickname');
 
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+const makeMessage = (type, payload) => {
+  return JSON.stringify({
+    type,
+    payload,
+  });
+};
 
 socket.addEventListener('open', () => {
   console.log('Connected to Browser ✅');
 });
 
 socket.addEventListener('message', (message) => {
-  console.log('New message:', message.data);
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener('close', () => {
@@ -18,8 +28,15 @@ socket.addEventListener('close', () => {
 const handleSubmit = (event) => {
   event.preventDefault();
   const input = messageForm.querySelector('input');
-  socket.send(input.value);
+  socket.send(makeMessage('new_message', input.value));
   input.value = '';
 };
 
+const handleNicknameSubmit = (event) => {
+  event.preventDefault();
+  const input = nicknameForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
+};
+
 messageForm.addEventListener('submit', handleSubmit);
+nicknameForm.addEventListener('submit', handleNicknameSubmit);
